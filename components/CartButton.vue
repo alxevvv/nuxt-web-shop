8f2fa4d9837fa6d1b7595ac27777e1da293ccd89
alxevvv <alxevvv@gmail.com>
2023-08-06 15:47:04 +0600

    dont show badge if there are no items in cart

--- components/CartButton.vue
+++ components/CartButton.vue
@@ -1,9 +1,10 @@
 <template>
   <nuxt-link to="/cart" class="mr-2">
     <v-btn icon>
-      <v-badge :content="badgeQuantity" color="success">
+      <v-badge v-if="badgeQuantity" :content="badgeQuantity" color="success">
         <v-icon>mdi-cart</v-icon>
       </v-badge>
+      <v-icon v-else>mdi-cart</v-icon>
     </v-btn>
   </nuxt-link>
 </template>
@@ -12,6 +13,6 @@
 const cartStore = useCartStore()
 
 const badgeQuantity = computed(() =>
-  cartStore.numItems > 9 ? "9+" : cartStore.numItems.toString()
+  cartStore.numItems > 9 ? "9+" : cartStore.numItems
 )
 </script>
