a442071a12a0cb24d8a76174c84d47ab2cfd9976
alxevvv <alxevvv@gmail.com>
2023-08-08 18:26:41 +0600

    search screen

---
+++ pages/search.vue
@@ -0,0 +1,35 @@
+<template>
+  <Title>Search</Title>
+
+  <v-container>
+    <v-row>
+      <v-col :md="3" cols="12">
+        <v-card class="pa-5">
+          <span>FILTERS</span>
+        </v-card>
+      </v-col>
+
+      <v-col :md="9" cols="12">
+        <v-row>
+          <v-col v-for="product in products" :key="product._id" :md="4">
+            <product-preview :product="product" />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import {q} from "groqd"
+import {Product} from "@/models"
+import {useSanityClient} from "@/composables"
+
+const sanity = useSanityClient()
+
+const {query} = q("*").filter("_type == 'product'")
+
+const {data: products} = await useAsyncData("products", () => {
+  return sanity.fetch<Product[]>(query)
+})
+</script>
