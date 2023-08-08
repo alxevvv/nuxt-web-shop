8566fa4a51c7b8a0d24595d162b8978606bd7990
alxevvv <alxevvv@gmail.com>
2023-08-08 22:01:01 +0600

    fix sanity auth token missing

--- composables/useSanityClient.ts
+++ composables/useSanityClient.ts
@@ -5,7 +5,10 @@ const client = createClient({
   dataset: "production",
   useCdn: true,
   apiVersion: "2023-08-05",
-  token: import.meta.env.SSR ? import.meta.env.SANITY_AUTH_TOKEN : undefined,
+  token:
+    import.meta.env.SSR === false
+      ? undefined
+      : import.meta.env.SANITY_AUTH_TOKEN,
 })
 
 function useSanityClient() {
